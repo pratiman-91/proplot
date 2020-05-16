dacf698078119eb8f4a4e8771bcd5ec4309a395d
Luke Davis <lukelbd@gmail.com>
2020-05-15 22:24:59 -0600

    Remove broken metaclass stuff

--- proplot/artist.py
+++ proplot/artist.py
@@ -3,7 +3,6 @@
 Create a metaclass
 """
 import matplotlib.artist as martist
-import matplotlib.lines as mlines
 
 __all__ = []
 
@@ -46,67 +45,7 @@ def _gen_properties(cls):
             yield name, property(*args, doc=getter.__doc__)
 
 
-# Approach #1: Apply properties to all artists
 for cls in _gen_subclasses(martist.Artist):
     for name, prop in _gen_properties(cls):
         setattr(cls, name, prop)
     print(f'Added properties to {cls.__name__}.')
-
-
-# Approach #2: Requires importing proplot before matplotlib.
-# This will not work for ProPlot but may be in matplotlib's future.
-class Propertifier(type):  # create new class definitions
-    """
-    Artist metaclass.
-    """
-    def __init__(cls, *args, **kwargs):
-        """
-        Add property getters and setters to all matplotlib artist subclasses.
-        """
-        super().__init__(*args, **kwargs)
-        for name, property in _gen_properties(cls):
-            setattr(cls, name, property)
-        print(f'Added properties to {cls.__name__}.')
-
-
-class Artist(martist.Artist, metaclass=Propertifier):
-    """
-    New base class for all matplotlib artists that
-    uses `Propertifier` as the metaclass.
-    """
-
-
-# Apply monkey patch
-import matplotlib.artist as martist
-martist.Artist = Artist
-
-
-# Testing metaclasses
-class MetaClass(type):
-    def __init__(cls, *args, **kwargs):
-        print('Brand new class!', cls)
-        super().__init__(*args, **kwargs)
-
-
-A = MetaClass('A', (object,), {})
-B = MetaClass('B', (A,), {})
-
-
-# Testing Artist
-class Test(Artist):
-    def set_foo(self, foo):
-        print('set foo!', foo)
-
-    def get_foo(self):
-        print('get foo!')
-
-    def get_bar(self):
-        print('get bar!')
-
-
-class Line2D(Artist, mlines.Line2D):
-    pass
-
-
-test = Test()
-line = Line2D([0], [0])
